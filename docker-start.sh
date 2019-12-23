f61a26f89a2b6b19aa843966c351c455eaccceb7
James Grundner <james@jgrundner.com>
2019-12-22 22:02:37 -0500

    Adding some shortcut scripts

---
+++ docker-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker-compose up -d nginx mysql phpmyadmin portainer mailhog
+
+#------------------------------------------------
+# If you have trouble logging into PhpMyAdmin
+#------------------------------------------------
+# go to mysql bash `docker-compose exec mysql bash`
+# login using root `mysql -u root -p` password is root.
+# then issue following mysql command `ALTER USER 'default' IDENTIFIED WITH mysql_native_password BY 'secret';`
+#
+#
